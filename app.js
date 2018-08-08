9d214f1bc074d990e9ce1ea9500d0a128226d0f2
Jana Bergant <jana.bergant@gmail.com>
2018-08-08 14:06:53 +0200

    s10-005-send-button-message

--- app.js
+++ app.js
@@ -210,6 +210,36 @@ function handleEcho(messageId, appId, metadata) {
 
 function handleDialogFlowAction(sender, action, messages, contexts, parameters) {
 	switch (action) {
+        case "faq-delivery":
+
+            handleMessages(messages, sender);
+
+            sendTypingOn(sender);
+
+            //ask what user wants to do next
+            setTimeout(function() {
+                let buttons = [
+                    {
+                        type:"web_url",
+                        url:"https://www.myapple.com/track_order",
+                        title:"Track my order"
+                    },
+                    {
+                        type:"phone_number",
+                        title:"Call us",
+                        payload:"+16505551234",
+                    },
+                    {
+                        type:"postback",
+                        title:"Keep on Chatting",
+                        payload:"CHAT"
+                    }
+                ];
+
+                sendButtonMessage(sender, "What would you like to do next?", buttons);
+            }, 3000)
+
+            break;
         case "detailed-application":
             if (isDefined(contexts[0]) &&
                 (contexts[0].name.includes('job_application') || contexts[0].name.includes('job-application-details_dialog_context'))
@@ -751,6 +781,10 @@ function receivedPostback(event) {
 	var payload = event.postback.payload;
 
 	switch (payload) {
+        case 'CHAT':
+            //user wants to chat
+            sendTextMessage(senderID, "I love chatting too. Do you have any other questions for me?");
+            break;
 		default:
 			//unindentified payload
 			sendTextMessage(senderID, "I'm not sure what you want. Can you be more specific?");
