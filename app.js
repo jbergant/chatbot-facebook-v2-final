8ef75cd719ce50bb18fc87cfe17038b85a3e93c0
Jana Bergant <jana.bergant@gmail.com>
2018-08-15 18:14:26 +0200

    S13-010–Facebook-Login-Authentication-implementation

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const uuid = require('uuid');
 const pg = require('pg');
 pg.defaults.ssl = true;
 
+const broadcast = require('./routes/broadcast');
+
 const userService = require('./services/user-service');
 const colors = require('./colors');
 const weatherService = require('./services/weather-service');
@@ -149,6 +151,8 @@ const usersMap = new Map();
 app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
 })
+
+app.use('/broadcast', broadcast);
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
