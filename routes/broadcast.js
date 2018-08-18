69f3488cb6f85015795922119bfe190e2b03c287
Jana Bergant <jana.bergant@gmail.com>
2018-08-18 20:09:26 +0200

    S13–016–Sending-simple-notifications

--- routes/broadcast.js
+++ routes/broadcast.js
@@ -31,11 +31,27 @@ router.post('/broadcast', ensureAuthenticated, function (req, res) {
 });
 
 router.get('/broadcast-send', ensureAuthenticated, function (req, res) {
+    let message = req.session.message;
+    let allUsers = req.session.users;
+
+    let sender;
+    for (let i=0; i < allUsers.length; i++ ) {
+        sender = allUsers[i].fb_id;
+        fbService.sendTextMessage(sender, message);
+    }
+
     res.redirect('/broadcast/broadcast-sent');
 });
 
 router.get('/broadcast-sent', ensureAuthenticated, function (req, res) {
-    res.render('broadcast-sent');
+    let newstype = req.session.newstype;
+    let message = req.session.message;
+    let users = req.session.users;
+
+    req.session.newstype = null;
+    req.session.message = null;
+    req.session.users = null;
+    res.render('broadcast-sent', {message: message, users: users, numUsers:users.length, newstype: newstype});
 });
 
 router.get('/logout', ensureAuthenticated, function (req, res) {
