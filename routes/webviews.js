b5b0329ceefda938b9438da8560914c4f1f5341f
Jana Bergant <jana.bergant@gmail.com>
2018-08-19 17:39:42 +0200

    s14-007-get-information-from-form

--- routes/webviews.js
+++ routes/webviews.js
@@ -2,6 +2,7 @@
 
 const config = require('../config');
 const express = require('express');
+const fbservice = require('../services/fb-service');
 
 const router = express.Router();
 
@@ -10,6 +11,13 @@ router.get('/webview', function (req, res) {
     res.render('newsletter-settings');
 });
 
+router.get('/save', function (req, res) {
+    let body = req.query;
+    let topics = body.topics.join(',');
+    let response = `Newsletter ${body.newsletter}, topics: ${topics} and deals ${body.deals}`;
 
+    // psid??
+    fbservice.sendTextMessage(body.psid, response);
+});
 
 module.exports = router;
