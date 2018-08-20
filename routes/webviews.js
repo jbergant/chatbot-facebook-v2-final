7385bc74c461168c436fefa286cc0fb89b7add8a
Jana Bergant <jana.bergant@gmail.com>
2018-08-20 08:13:27 +0200

    s14-010-save-settings-to-database

--- routes/webviews.js
+++ routes/webviews.js
@@ -6,6 +6,8 @@ const fbservice = require('../services/fb-service');
 
 const router = express.Router();
 
+const pg = require('pg');
+pg.defaults.ssl = true;
 
 router.get('/webview', function (req, res) {
     res.render('newsletter-settings');
@@ -14,11 +16,26 @@ router.get('/webview', function (req, res) {
 router.get('/save', function (req, res) {
     let body = req.query;
     let topics = body.topics.join(',');
-    let response = `Newsletter ${body.newsletter}, topics: ${topics} and deals ${body.deals} 
-    for psid ${body.psid}`;
-
-    // psid??
-    fbservice.sendTextMessage(body.psid, response);
+    let pool = new pg.Pool(config.PG_CONFIG);
+    pool.connect(function (err, client, done) {
+        if (err) {
+            return console.error('Error acquiering client');
+        }
+        client.query("UPDATE public.users SET newsletter=$1, topics=$2, deals=$3 WHERE fb_id=$4",
+            [
+                body.newsletter,
+                topics,
+                body.deals,
+                body.psid
+            ],
+            function (err, result) {
+                if(err === null) {
+                    fbservice.sendTextMessage(body.psid, 'Settings saved.');
+                } else {
+                    console.log('ERR: ' + err);
+                }
+            });
+    });
 });
 
 module.exports = router;
