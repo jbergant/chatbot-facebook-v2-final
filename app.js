2b7eff8ce6f05d49d4ee53a423e569fe218f1d75
Jana Bergant <jana.bergant@gmail.com>
2018-11-20 20:15:42 +0100

    s12-004-save_user_into-db

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 const uuid = require('uuid');
+const pg = require('pg');
+pg.defaults.ssl = true;
 
 
 // Messenger API parameters
@@ -46,6 +48,9 @@ if (!config.EMAIL_TO) { //sending email
 }
 if (!config.WEATHER_API_KEY) { //weather api key
     throw new Error('missing WEATHER_API_KEY');
+}
+if (!config.PG_CONFIG) { //pg config
+    throw new Error('missing PG_CONFIG');
 }
 
 app.set('port', (process.env.PORT || 5000))
@@ -797,8 +802,35 @@ function greetUserText(userId) {
 			var user = JSON.parse(body);
 			console.log('getUserData: ' + user);
 			if (user.first_name) {
-				console.log("FB user: %s %s, %s",
-					user.first_name, user.last_name, user.profile_pic);
+
+                var pool = new pg.Pool(config.PG_CONFIG);
+                pool.connect(function(err, client, done) {
+                    if (err) {
+                        return console.error('Error acquiring client', err.stack);
+                    }
+                    var rows = [];
+                    client.query(`SELECT fb_id FROM users WHERE fb_id='${userId}' LIMIT 1`,
+                        function(err, result) {
+                            if (err) {
+                                console.log('Query error: ' + err);
+                            } else {
+
+                                if (result.rows.length === 0) {
+                                    let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic) ' +
+										'VALUES ($1, $2, $3, $4)';
+                                    client.query(sql,
+                                        [
+                                            userId,
+                                            user.first_name,
+                                            user.last_name,
+                                            user.profile_pic
+                                        ]);
+                                }
+                            }
+                        });
+
+                });
+                pool.end();
 
 				sendTextMessage(userId, "Welcome " + user.first_name + '! ' +
                     'I can answer frequently asked questions for you ' +
@@ -886,36 +918,6 @@ function receivedPostback(event) {
 	console.log("Received postback for user %d and page %d with payload '%s' " +
 		"at %d", senderID, recipientID, payload, timeOfPostback);
 
-}
-
-
-function greetUserText(userId) {
-    //first read user firstname
-    request({
-        uri: 'https://graph.facebook.com/v2.7/' + userId,
-        qs: {
-            access_token: config.FB_PAGE_TOKEN
-        }
-
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-
-            var user = JSON.parse(body);
-
-            if (user.first_name) {
-                console.log("FB user: %s %s, %s",
-                    user.first_name, user.last_name, user.gender);
-
-                sendTextMessage(userId, "Welcome " + user.first_name + '!');
-            } else {
-                console.log("Cannot get data for fb user with id",
-                    userId);
-            }
-        } else {
-            console.error(response.error);
-        }
-
-    });
 }
 
 
